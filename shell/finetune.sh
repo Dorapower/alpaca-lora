42800aaa3dc66f8bddd910ace6047aef9212dfad
Dorapower <thatisluo@outlook.com>
2023-09-13 17:32:20 +0800

    feat: ft code

--- shell/finetune.sh
+++ shell/finetune.sh
@@ -4,9 +4,9 @@ cd /export/home/personal/luobaojia1/PycharmProjects/alpaca-lora || exit
 
 python finetune.py \
     --base_model './models/llama-7b-hf' \
-    --data_path './datasets/refined.jsonl' \
+    --data_path './datasets/alpaca.jsonl' \
     --prompt_template_name 'refiner' \
-    --output_dir './models/lora-alpaca' \
+    --output_dir './lora-models/lora-alpaca' \
     --batch_size 256 \
     --micro_batch_size 16 \
     --num_epochs 5
---
+++ shell/finetune_dist.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd /export/home/personal/luobaojia1/PycharmProjects/alpaca-lora || exit
+
+torchrun \
+    --standalone \
+    --nnodes 1 \
+    --nproc-per-node 8 \
+    finetune.py \
+    --base_model './models/llama-7b-hf' \
+    --data_path './datasets/alpaca.jsonl' \
+    --prompt_template_name 'refiner' \
+    --output_dir './lora-models/lora-alpaca' \
+    --batch_size 256 \
+    --micro_batch_size 16 \
+    --num_epochs 5
